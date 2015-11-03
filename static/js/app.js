fa60e84a625a8345e3409c62ac2595e0e022c03d
yupswing <simonecingano@gmail.com>
2015-11-03 18:43:05 +0100

    UI; Dettaglio chiamate

--- static/js/app.js
+++ static/js/app.js
@@ -15,6 +15,32 @@ $(function () {
 
   $('body').find(withRipples).ripples();
 
+	var icons = {
+			time: 'zmdi zmdi-time',
+			date: 'zmdi zmdi-calendar',
+			up: 'zmdi zmdi-chevron-up',
+			down: 'zmdi zmdi-chevron-down',
+			previous: 'zmdi zmdi-chevron-left',
+			next: 'zmdi zmdi-chevron-right',
+			today: 'zmdi zmdi-dot-circle-alt',
+			clear: 'zmdi zmdi-delete',
+			close: 'zmdi zmdi-close'
+	};
+
+	$('input.timepicker').datetimepicker({
+										icons: icons,
+                    format: 'HH:mm',
+										locale: 'it'
+                }).show();
+
+	$('input.datepicker').datetimepicker({
+										// inline: true,
+										// sideBySide: true,
+										icons: icons,
+                    format: 'DD-MM-YYYY',
+										locale: 'it'
+                }).show();
+
   $('.form-control').each(function (){
 		console.log($(this));
     // Add class filled to form-control's that have a value
