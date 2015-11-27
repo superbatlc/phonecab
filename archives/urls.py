73377b02b83dd3f0b860a8a44911d7e6da8f1dcb
Alessandro <kappaciccio@gmail.com>
2015-11-27 17:25:47 +0100

    Pulsanti esportazione anagrafiche nascosti

--- archives/urls.py
+++ archives/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     url(r'^archives/phoneusers/$', archive_phoneuser_home),
     url(r'^archives/phoneusers/view/(?P<archived_phoneuser_id>[0-9]*)$', archive_phoneuser_view),
+    #url(r'^archives/phoneusers/export/excel/$', archive_phoneusers_export_excel),
     url(r'^archives/whitelists/$', archive_whitelist_items),
     url(r'^archives/cdrs/$', archive_cdrs_home),
     url(r'^archives/records/$', archive_records_home),
