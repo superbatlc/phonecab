8737c836a61fb8dc4e05d126fb0606b0af727699
Alessandro <kappaciccio@gmail.com>
2016-02-29 17:11:31 +0100

    Dev; Aggiunto controllo occupazione disco esterno

--- tools/views.py
+++ tools/views.py
@@ -14,29 +14,32 @@ def tools_home(request):
     user = request.user
     variables = Acl.get_permissions_for_user(user.id, user.is_staff)
     variables['diskusage'] = 0
+    variables['extdiskusage'] = 0
     if request.GET.get("err") != '1':
         try:
-            variables['diskusage'] = _tool_get_disk_usage()
+            variables['diskusage'] = _tool_get_disk_usage(settings.FILESYSTEM)
+            variables['extdiskusage'] = _tool_get_disk_usage(settings.EXT_FILESYSTEM)
             variables['peers_status'] = _get_peers_status()
         except Exception as e:
             return redirect("/tools/?err=1&err_msg=%s" % format(e))
-            #return redirect("/tools/?err=1&err_msg=Impossibile recuperare il valore di occupazione disco")
-    
+            #return redirect("/tools/?err=1&err_msg=Impossibile recuperare il valore di occupazione disco"
     return render_to_response(
         'tools/home.html', RequestContext(request, variables))
 
 
-def _tool_get_disk_usage():
+def _tool_get_disk_usage(disk):
     import os
     percent = os.popen("df -hl | grep '%s' | awk 'BEGIN{}{percent=$5} END{print percent}'" % settings.FILESYSTEM).read()
-    return float(percent.replace("%", ""))
+    if percent:
+        return float(percent.replace("%", ""))
+    return 0
 
 def _get_peers_status():
 
     extensions = Extension.objects.all().order_by('extension')
     stati = []
     print extensions
-    
+
     for extension in extensions:
         stato = 'OFF'
         status = os.popen('sudo /usr/sbin/asterisk -rx "sip show peer %s" | grep "Status"' % extension.extension).read()
