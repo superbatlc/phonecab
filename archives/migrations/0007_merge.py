20380bbf4a09209c941d00a5f10be56f16a855f1
Alessandro <kappaciccio@gmail.com>
2016-03-01 10:51:57 +0100

    Merge migration

---
+++ archives/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('archives', '0006_auto_20160128_1217'),
+        ('archives', '0006_auto_20160301_0808'),
+    ]
+
+    operations = [
+    ]
