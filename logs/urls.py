08164f58ca8d8b412a31932a016145ceab3fd058
Alessandro <kappaciccio@gmail.com>
2016-06-09 17:25:39 +0200

    Dev; Template updates to better logs visualization

--- logs/urls.py
+++ logs/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
    url(r'^logs/last/$', get_last_logs),
+   url(r'^logs/last/(?P<pk>[0-9]*)/$', get_last_logs),
 ]
--- logs/views.py
+++ logs/views.py
@@ -1,12 +1,24 @@
-from django.http import HttpResponse
-from .models import Log
 import json
+import datetime
+from django.http import HttpResponse
 from django.core import serializers
+from .models import Log
+
+
 
 # Create your views here.
-def get_last_logs(request,):
-    max_logs_returned = 10
-    logs = Log.objects.all().order_by('-when')[:max_logs_returned]
+def get_last_logs(request, pk=None):
+
+    if pk:
+        logs = Log.objects.filter(pk__gt=pk).order_by('when')
+    else:
+        today = datetime.datetime.today()
+        midnight = today.replace(hour=0, minute=0, second=0)
+
+        logs = Log.objects.filter(when__gte=midnight).order_by('when')
+
+    for log in logs:
+        log.kind_str = log.get_kind_display()
 
     return HttpResponse(status=200,
                         content=serializers.serialize('json', logs),
